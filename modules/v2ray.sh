1a7a17694c27d605560cfd5ca79d0291482808e1
Mohammad Developer <MohammadDeveloperMail@gmail.com>
2023-06-13 20:25:14 +0330

    fix bugs

--- modules/v2ray.sh
+++ modules/v2ray.sh
@@ -15,7 +15,7 @@ function change_port {
 
 function set_firewall {
     if [ "$(command -v ufw)" == 0 ]; then 
-        pkg install ufw 2&>1 /dev/null | echo -e "[!] Installing UFW Firewall"
+        pkg install ufw 2>&1 /dev/null | echo -e "[!] Installing UFW Firewall"
         ufw allow 80/tcp && ufw allow 443/tcp && ufw allow $SSH_PORT/tcp | echo -e "[!] Opening 80, 443, $SSH_PORT ports"
     else
         ufw allow 80/tcp && ufw allow 443/tcp && ufw allow $SSH_PORT/tcp | echo -e "[!] Opening 80, 443, $SSH_PORT ports"
@@ -23,8 +23,8 @@ function set_firewall {
 }
 
 function install_package {
-    pkg update && pkg upgrade -y 2&>1 /dev/null | echo -e "[+] Updating Package Manager"
-    pkg install docker docker-compose certbot curl wget -y 2&>1 /dev/null | echo -e "[+] Installing needed tools"
+    pkg update && pkg upgrade -y 2>&1 /dev/null | echo -e "[+] Updating Package Manager"
+    pkg install docker docker-compose certbot curl wget -y 2>&1 /dev/null | echo -e "[+] Installing needed tools"
 }
 
 function tls_domains {
@@ -35,14 +35,14 @@ function tls_domains {
 }
 
 function create_and_change_user {
-    useradd -mG sudo $USER_NAME -s /bin/bash 2&>1 /dev/null | echo -e "[+] Creating new user"
+    useradd -mG sudo $USER_NAME -s /bin/bash 2>&1 /dev/null | echo -e "[+] Creating new user"
     echo -e "Set password for new user"
     echo $USER_PASS | passwd $USER_NAME
     su $USER_NAME | echo -e "[!] Switching to new user"
 }
 
 function install_v2ray {
-    curl -O https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh 2&>1 /dev/null | echo -e "[+] Downloading 3XUI installation script"
+    curl -O https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh 2>&1 /dev/null | echo -e "[+] Downloading 3XUI installation script"
     chmod +x install.sh | echo -e "[!] Accessing to script"
     echo 'y' | echo $PANEL_USER | echo $PANEL_PASSWORD | echo $PANEL_PORT | sudo ./install.sh | echo -e "[!] Installing and configuring 3XUI"
     rm install.sh
